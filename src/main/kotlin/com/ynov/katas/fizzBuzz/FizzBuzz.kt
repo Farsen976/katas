6316527e6e7240b551de3553d5dbd29a9482dce9
Farsen976 <farsen.abdallahcombo@ynov.com>
2020-03-06 12:15:27 +0100

    green: add code

--- src/main/kotlin/com/ynov/katas/fizzBuzz/FizzBuzz.kt
+++ src/main/kotlin/com/ynov/katas/fizzBuzz/FizzBuzz.kt
@@ -2,6 +2,21 @@ package com.ynov.katas.fizzBuzz
 
 class FizzBuzz {
     fun execute(number: Int): String {
-        return ""
+        if (divisibleBy5And3(number) == 0) {
+            return  "FizzBuzz"
+        }
+
+       if (divisibleBy5(number) == 0) {
+           return "Buzz"
+       }
+        if (divisibleBy3(number) == 0){
+            return "Fizz"
+        }
+        return "$number"
     }
+
+    private fun divisibleBy3(number: Int): Int =  number % 3
+    private fun divisibleBy5(number: Int): Int = number % 5
+    private fun divisibleBy5And3(number: Int): Int = divisibleBy5(number) + divisibleBy3(number)
+
 }
