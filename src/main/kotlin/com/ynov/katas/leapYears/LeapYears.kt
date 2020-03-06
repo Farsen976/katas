47e6344c0baa1a548ee304115a1b6c39a0d110c2
Farsen976 <farsen.abdallahcombo@ynov.com>
2020-03-06 14:24:19 +0100

    red(LeapYear): all LeapYear tests cases

---
+++ src/main/kotlin/com/ynov/katas/leapYears/LeapYears.kt
@@ -0,0 +1,7 @@
+package com.ynov.katas.leapYears
+
+class LeapYears {
+    fun execute(year: Int): String {
+        return ""
+    }
+}
---
+++ src/test/kotlin/com/ynov/katas/leapYears/LeapYearsTest.kt
@@ -0,0 +1,31 @@
+package com.ynov.katas.leapYears
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+internal class LeapYearsTest {
+
+    @Test
+    fun`should be leap year if it's divisible by 400`(){
+        val leapYears = LeapYears()
+        assertEquals("Leap Year", leapYears.execute(200))
+    }
+
+    @Test
+    fun`should not be leap year if it's divisible by 100 but not by 400`(){
+        val leapYears = LeapYears()
+        assertEquals("Not Leap Year", leapYears.execute(1700))
+    }
+
+    @Test
+    fun`should be leap year if it's divisible by 4 but not by 100`(){
+        val leapYears = LeapYears()
+        assertEquals("Leap Year", leapYears.execute(2008))
+    }
+
+    @Test
+    fun`should not be leap year if it's not divisible by 4`(){
+        val leapYears = LeapYears()
+        assertEquals("Not Leap Year", leapYears.execute(2019))
+    }
+}
