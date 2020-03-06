6688256af7b205ef2b93d255d2ecf3c891a59a4b
Farsen976 <farsen.abdallahcombo@ynov.com>
2020-03-06 15:05:52 +0100

    refactor(LeapYear): add constants

--- src/main/kotlin/com/ynov/katas/leapYears/LeapYears.kt
+++ src/main/kotlin/com/ynov/katas/leapYears/LeapYears.kt
@@ -2,8 +2,12 @@ package com.ynov.katas.leapYears
 
 class LeapYears {
     fun execute(year: Int): String {
-        if(year.rem(400) == 0) return "Leap Year"
-        if(year.rem(4) == 0 && year.rem(100) != 0) return "Leap Year"
-        return "Not Leap Year"
+        if(year.rem(400) == 0) return LEAP_YEAR
+        if(year.rem(4) == 0 && year.rem(100) != 0) return LEAP_YEAR
+        return NOT_LEAP_YEAR
+    }
+    companion object{
+        const val LEAP_YEAR = "Leap Year"
+        const val NOT_LEAP_YEAR = "Not Leap Year"
     }
 }
